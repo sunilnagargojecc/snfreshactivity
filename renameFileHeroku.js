57ad292b6b062ffad41bab6f19c955b307d6abeb
sunilnagargojecc <59916458+sunilnagargojecc@users.noreply.github.com>
2020-02-13 20:55:14 +0530

    Add files via upload

---
+++ renameFileHeroku.js
@@ -0,0 +1,21 @@
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+
+sftp.connect({
+  host: process.env.FTP_HOST,
+  port: process.env.FTP_PORT,
+  username: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD
+}).then(() => {
+	// Push SMS file to ICS SFTP /upload/IN Folder
+	 return sftp.rename(process.env.Source_File,process.env.Dest_File)
+	 	
+  //return sftp.list('/ccengage/After30');
+}).then(() => {
+  console.log('Closing SFTP');
+   //return sftp.rename('/ccengage/After30/Sunil.txt','/ccengage/After30/Sunil.csv')
+   return sftp.end();
+   
+}).catch(err => {
+  console.log(err, 'catch error');
+});
