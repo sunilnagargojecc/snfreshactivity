7789c51ea5b25baae3c10eaf22c541f28ba27d20
sunilnagargojecc <59916458+sunilnagargojecc@users.noreply.github.com>
2020-02-14 15:27:01 +0530

    Add files via upload

---
+++ renameHRK.js
@@ -0,0 +1,13 @@
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+sftp.connect({
+  host: process.env.FTP_HOST,
+  port: process.env.FTP_PORT,
+  username: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD
+}).then(() => {
+	console.log('connected...');
+	return sftp.end();
+}).catch(err => {
+  console.log(err, 'catch error');
+})
